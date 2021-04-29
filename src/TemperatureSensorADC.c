124921de2a052c8cd69c027bf5d5744fcf3f5bb0
Hemanth A - 256889 <57669802+hemanthasapu@users.noreply.github.com>
2021-04-29 23:59:52 +0530

    upload

--- src/TemperatureSensorADC.c
+++ src/TemperatureSensorADC.c
@@ -16,8 +16,8 @@
  * 
  */
 void Init_ADC(){
-    ADMUX = (1<<REFS0);
-    ADCSRA = (1<<ADEN)|(7<<ADPS0);
+    SET_REFERENCE_VOLTAGE;
+    ADC_ENABLE_PRESCALE;
 
 }
 
@@ -35,8 +35,8 @@ uint16_t Read_ADC(uint8_t ch)
 
     ADCSRA|=(1<<ADSC);
 
-    while(!(ADCSRA & (1<<ADIF)));
-    ADCSRA|=(1<<ADIF);
+    while(ADC_INTERRUPT_NOT_OCCURED);
+    SET_ADC_INTERRUPT_FLAG;
     return(ADC);
 
 }
