5337299aac6eb5e1d19f1da47bd4a5e09b8f64ee
Hemanth A - 256889 <57669802+hemanthasapu@users.noreply.github.com>
2021-04-29 17:48:36 +0530

    updateed mcros

--- src/SerialCommunicationUART.c
+++ src/SerialCommunicationUART.c
@@ -19,9 +19,8 @@ void UARTinit(uint16_t ubrr_value){
 
     UBRR0L = ubrr_value;
     UBRR0H = (ubrr_value>>8)&(0x00ff);
-    UCSR0C = (1<<UMSEL00)|(1<<UCSZ00)|(1<<UCSZ01);
-
-    UCSR0B = (1<<RXEN0)|(1<<TXEN0)|(1<<RXCIE0)|(1<<TXCIE0);
+    UART_CHARACTER_SIZE; // 8 bit size of data
+    UART_ENABLED; //enable rx and tx of uart with interrupts
 
 }
 
